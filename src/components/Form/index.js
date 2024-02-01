bdea5b280d525fb9f66414d599088703ad0ddb96
PedroDiSanti <pedroa.disanti@gmail.com>
2024-02-01 20:42:56 -0300

    Adicionando dropdown

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,18 @@
 import './Form.css'
 import TextField from '../TextField'
+import DropDown from '../DropDown'
 
 const Form = () => {
+    const times = [
+        'Programação',
+        'Frontend',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'        
+    ]
+
     return (
         <section className='form'>
             <form>
@@ -9,6 +20,7 @@ const Form = () => {
                 <TextField label="Nome" placeholder="Digite seu nome" />
                 <TextField label="Cargo" placeholder="Digite seu cargo" />
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
+                <DropDown label="Time" itens={times}/>
             </form>
         </section>
     )
